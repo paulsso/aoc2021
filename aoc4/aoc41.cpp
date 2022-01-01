10f26a424eb2eca599e9e277071c269cd2f82f34
oskar <oskar@paulsson.dev>
2022-01-01 20:48:24 +0100

    added function vectorizeStrToInt to handle the row of drawn bingo numbers

--- aoc4/aoc41.cpp
+++ aoc4/aoc41.cpp
@@ -1,32 +1,83 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
+#include <algorithm>
+#include <typeinfo>
+#include <ctype.h>
 
+typedef struct meta {
+  bool marked;
+  int number;
+  int row;
+  int col;
+} meta;
 
-void readInput(std::vector<std::string> &input, const std::string file_name);
+typedef std::vector<std::vector<meta>> matrix;
 
-void readInput(std::vector<std::string> &input, const std::string file_name) {
+long long VectorToInt(std::vector<int>);
+
+void readInput(std::vector<int> &draw_numbers, matrix &boards,
+  const std::string file_name);
+
+std::vector<int> vectorizeStrToInt(std::vector<std::string> &input,
+    std::vector<int> &output);
+
+std::vector<int> vectorizeStrToInt(std::vector<std::string> &input,
+    std::vector<int> &output)
+{
+    std::string str;
+    std::vector<int> v;
+    for ( auto c : input[0] ){
+        if ( isdigit(c) ) { str+=c; }
+        else { v.push_back(std::stoi(str)); str.clear(); }
+    }
+    output = v;
+}
+
+void readInput(std::vector<int> &draw_numbers, matrix &boards,
+  const std::string file_name) {
 
     std::cout << "Reading from file: " << file_name << std::endl;
     std::string line;
+    std::vector<std::string> input1;
+    std::vector<std::string> input2;
     std::fstream infile(file_name);
+    int lineNo = 0;
 
-    while ( getline(infile,line) ) { 
-        input.push_back(line);
+    while ( getline(infile,line) )
+    {
+        input1.push_back(line);
+        std::cout << line << '\n' << std::endl;
+        lineNo += 1;
+        if (lineNo > 0) { break; }
     }
+    while ( getline(infile,line) )
+    {
+        if (lineNo > 1) {
+            input2.push_back(line);
+            std::cout << line << std::endl;
+        }
+        lineNo += 1;
+    }
+    // get the first line, these are the bingo numbers, csv
+    // the remaining numbers are all the bingo boards, put them in the matrix
+    // use the meta struct to see check for bingo
+    vectorizeStrToInt(input1, draw_numbers);
 
     infile.close();
 }
 
-int main() 
+int main()
 {
     std::string file_name;
-    std::vector<std::string> input;
-
+    std::vector<int> draw_numbers;
+    matrix bingo_boards;
     std::cin >> file_name;
-    
-    readInput(input, file_name);
-    
+
+    readInput(draw_numbers, bingo_boards, file_name);
+    printf("print draw numbers: \n");
+    for (auto n : draw_numbers) { std::cout << n << ','; }
+    std::cout<<'\n';
     return 0;
 }
-
